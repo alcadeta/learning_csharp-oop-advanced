8f38359ef987620e5083f4f8d0946fb74ee8e888
Alican Demirtas <demirtasdev@hotmail.com>
2021-05-30 11:22:18 +0100

    Theory vs Practice: When A Square is Not A Rectangle

---
+++ TheoryVsPractice/Rectangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TheoryVsPractice
+{
+    /// <summary>
+    /// Not all relationships, as they are defined in theory or even the real
+    /// world, directly translate to good relationships when it comes to
+    /// relations within an object-oriented design.
+    /// </summary>
+
+    public class Rectangle
+    {
+        public uint Width { get; set; }
+        public uint Height { get; set; }
+    }
+
+    public class Square // : Rectangle
+    {
+        public uint Width { get; set; }
+    }
+}
